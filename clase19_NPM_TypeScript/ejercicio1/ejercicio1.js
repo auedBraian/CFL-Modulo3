a65072dc2a14e4255661aa78472dac0b58ea39bd
Braian Aued <braianaued@gmail.com>
2020-07-11 15:20:49 -0300

    Clase 19 Ejercicios NPM

---
+++ clase19_NPM_TypeScript/ejercicio1/ejercicio1.js
@@ -0,0 +1,4 @@
+ 
+var myApp = require("simple-hello-world-example");
+ 
+var msg = myApp.printMsg(); // print and return " Hello World! " 
---
+++ clase19_NPM_TypeScript/ejerciciosEnClase/simpleHelloWorld.js
@@ -0,0 +1,6 @@
+
+var myApp = require("simple-hello-world-example");
+ 
+var msg = myApp.printMsg(); // print and return " Hello World! " 
+
+console.log("Servidor activado");
