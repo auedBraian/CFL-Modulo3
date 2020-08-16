3231fa93d4df610f0c46b6fabcdabc5840d4a56a
Braian Aued <braianaued@gmail.com>
2020-08-16 14:58:00 -0300

    registro automotor comentado

--- clase22_Encapsulamiento/registroAutomotorConGestorArch/registro.js
+++ clase22_Encapsulamiento/registroAutomotorConGestorArch/registro.js
@@ -1,6 +1,7 @@
 "use strict";
 exports.__esModule = true;
 var fs = require("fs");
+//Clase Auto con sus variables y metodos
 var readlineSync = require('readline-sync');
 var Auto = /** @class */ (function () {
     function Auto(brand, model, year, color, patente) {
@@ -40,6 +41,7 @@ var Auto = /** @class */ (function () {
     Auto.prototype.setCarRegistration = function (registration) {
         this.carRegistration = registration;
     };
+    //funcion que recibe 2 elementos del tipo Auto y compara si son iguales. Si son iguales devuelve true
     Auto.prototype.compare = function (auto1, auto2) {
         if (auto1.getBrand() == auto2.getBrand() && auto1.getModel() == auto2.getModel() && auto1.getYear() == auto2.getYear() && auto1.getColor() == auto2.getColor() && auto1.getCarRegistration() == auto2.getCarRegistration()) {
             return true;
@@ -47,6 +49,7 @@ var Auto = /** @class */ (function () {
     };
     return Auto;
 }());
+//Clase AutomotorRegistry con sus variables y metodos
 var AutomotorRegistry = /** @class */ (function () {
     function AutomotorRegistry(autos) {
         this.autos = [];
@@ -57,6 +60,7 @@ var AutomotorRegistry = /** @class */ (function () {
             this.autos = autos;
         }
     }
+    //funcion que recibe un elemento de tipo Auto y busca en el registro si el elemento esta. Si esta devuelve la posicion del mismo. Para buscarlo utiliza la funcion compare de la clase Auto
     AutomotorRegistry.prototype.searchAuto = function (auto) {
         var position = -1;
         for (var i = 0; i < this.autos.length; i++) {
@@ -73,6 +77,7 @@ var AutomotorRegistry = /** @class */ (function () {
         }
         return position;
     };
+    //funcion que recibe una patente, y busca en el registro si algun elemento del tipo Auto posee esa patente. Si esta devuelve la posicion de ese elemento, sino devuelve -1
     AutomotorRegistry.prototype.searchAutobyCarRegistration = function (carReg) {
         var position = -1;
         for (var i = 0; i < this.autos.length; i++) {
@@ -88,6 +93,7 @@ var AutomotorRegistry = /** @class */ (function () {
         }
         return position;
     };
+    //funcion que recibe un elemento de tipo Auto, lo busca en el registro y si esta lo elimina
     AutomotorRegistry.prototype.deleteAuto = function (auto) {
         var position = -1;
         position = this.searchAuto(auto);
@@ -97,6 +103,7 @@ var AutomotorRegistry = /** @class */ (function () {
             this.autos.splice(position, 1);
         }
     };
+    //funcion que recibe un elemento de tipo Auto y lo inserta en el registro
     AutomotorRegistry.prototype.insertAuto = function (auto) {
         this.autos.push(auto);
     };
@@ -105,6 +112,7 @@ var AutomotorRegistry = /** @class */ (function () {
     };
     return AutomotorRegistry;
 }());
+/*Clase FileManager, tiene los metodos para recibir informacion de un txt y convertir esa informacion a objetos de tipo Auto*/
 var FileManager = /** @class */ (function () {
     function FileManager(location) {
         var txtFile = fs.readFileSync(location, 'utf8');
@@ -118,6 +126,7 @@ var FileManager = /** @class */ (function () {
     };
     return FileManager;
 }());
+//Funcion que recibe un arreglo de strings, los transforma en elementos del tipo Auto, los insertar en el registro automotor que es un arreglo de elementos de tipo Auto
 function completeRegistry(arrayCars) {
     var registry = [];
     for (var i = 0; i < arrayCars.length; i++) {
@@ -128,6 +137,7 @@ function completeRegistry(arrayCars) {
     }
     return registry;
 }
+//funcion que crea un nuevo auto y lo devuelve
 function completeCar() {
     var brand = readlineSync.question("insert brand ");
     var model = readlineSync.question("insert model ");
@@ -141,6 +151,7 @@ var car = 'automotorList.txt';
 var txtFile = new FileManager(car);
 var cars = [];
 cars = txtFile.getStringArray();
+//creo el registro automotor, y la funcion completeRegisty va a generar el arreglo de elementos de tipo Auto para llenar ese registro
 var registry = new AutomotorRegistry(completeRegistry(cars));
 var option = 0;
 console.log(registry);
@@ -148,24 +159,24 @@ option = readlineSync.questionInt("Wich option do you want to use? press 1 for s
 while (option != 0) {
     var auto1 = void 0;
     if (option == 1) {
-        auto1 = completeCar();
-        registry.searchAuto(auto1);
+        auto1 = completeCar(); //creo un nuevo auto
+        registry.searchAuto(auto1); //envio el auto creado al registro para ver si coincide con alguno de los elementos del registro
     }
     else if (option == 2) {
         var carReg = readlineSync.question("insert car registration ");
         carReg.toUpperCase;
-        registry.searchAutobyCarRegistration(carReg);
+        registry.searchAutobyCarRegistration(carReg); //busco un elemento en el registro con la patente del elemento
     }
     else if (option == 3) {
         auto1 = completeCar();
-        registry.insertAuto(auto1);
+        registry.insertAuto(auto1); //inserto un elemento Auto en el registro
     }
     else if (option == 4) {
         auto1 = completeCar();
-        registry.deleteAuto(auto1);
+        registry.deleteAuto(auto1); //busco un elemento del registro y lo elimino
     }
     else if (option == 5) {
-        console.log(registry);
+        console.log(registry); // imprimo el registro completo
     }
     option = readlineSync.questionInt("Wich option do you want to use? press 1 for search by features,2 for search by car registration, 3 for insert, 4 for delete, 5 for print all the registy, 0 for exit ");
 }
--- clase22_Encapsulamiento/registroAutomotorConGestorArch/registro.ts
+++ clase22_Encapsulamiento/registroAutomotorConGestorArch/registro.ts
@@ -1,7 +1,7 @@
 
 import * as fs from 'fs';
 
-
+//Clase Auto con sus variables y metodos
 var readlineSync = require('readline-sync');
 
 class Auto {
@@ -62,7 +62,7 @@ class Auto {
         this.carRegistration = registration;
     }
 
-
+//funcion que recibe 2 elementos del tipo Auto y compara si son iguales. Si son iguales devuelve true
     public compare(auto1: Auto, auto2: Auto): boolean {
 
     if (auto1.getBrand() == auto2.getBrand() && auto1.getModel() == auto2.getModel() && auto1.getYear() == auto2.getYear() && auto1.getColor() == auto2.getColor() && auto1.getCarRegistration() == auto2.getCarRegistration()) {
@@ -77,7 +77,7 @@ class Auto {
 
 
 
-
+//Clase AutomotorRegistry con sus variables y metodos
 class AutomotorRegistry {
 
     private autos: Auto[] = [];
@@ -93,6 +93,7 @@ class AutomotorRegistry {
         }
     }
 
+    //funcion que recibe un elemento de tipo Auto y busca en el registro si el elemento esta. Si esta devuelve la posicion del mismo. Para buscarlo utiliza la funcion compare de la clase Auto
     searchAuto(auto: Auto): number {
 
         let position: number = -1;
@@ -115,7 +116,8 @@ class AutomotorRegistry {
         return position;
     }
 
-    
+
+    //funcion que recibe una patente, y busca en el registro si algun elemento del tipo Auto posee esa patente. Si esta devuelve la posicion de ese elemento, sino devuelve -1
         searchAutobyCarRegistration(carReg: string): number {
 
         let position: number = -1;
@@ -137,7 +139,8 @@ class AutomotorRegistry {
         return position;
     }
     
-
+    
+    //funcion que recibe un elemento de tipo Auto, lo busca en el registro y si esta lo elimina
     deleteAuto(auto: Auto): void {
 
         let position: number = -1;
@@ -154,7 +157,7 @@ class AutomotorRegistry {
 
     }
 
-
+    //funcion que recibe un elemento de tipo Auto y lo inserta en el registro
     insertAuto(auto: Auto): void {
 
         this.autos.push(auto);
@@ -168,6 +171,7 @@ class AutomotorRegistry {
 }
 
 
+/*Clase FileManager, tiene los metodos para recibir informacion de un txt y convertir esa informacion a objetos de tipo Auto*/
 class FileManager {
     
     private stringArray: string[];
@@ -188,7 +192,7 @@ class FileManager {
 
 
 
-
+//Funcion que recibe un arreglo de strings, los transforma en elementos del tipo Auto, los insertar en el registro automotor que es un arreglo de elementos de tipo Auto
 function completeRegistry(arrayCars: string[]): Auto[] {
 
     let registry: Auto[] = [];
@@ -202,6 +206,7 @@ function completeRegistry(arrayCars: string[]): Auto[] {
     return registry;
 }
 
+//funcion que crea un nuevo auto y lo devuelve
 function completeCar(): Auto {
 
     let brand: string = readlineSync.question("insert brand ");
@@ -219,6 +224,7 @@ let txtFile:FileManager = new FileManager(car);
 let cars: string[] = [];
 cars= txtFile.getStringArray();
 
+//creo el registro automotor, y la funcion completeRegisty va a generar el arreglo de elementos de tipo Auto para llenar ese registro
 let registry: AutomotorRegistry = new AutomotorRegistry(completeRegistry(cars));
 
 let option: number = 0;
@@ -231,25 +237,25 @@ while (option != 0) {
 
     let auto1: Auto;
     if (option == 1){
-        auto1 = completeCar();
-        registry.searchAuto(auto1);
+        auto1 = completeCar(); //creo un nuevo auto
+        registry.searchAuto(auto1); //envio el auto creado al registro para ver si coincide con alguno de los elementos del registro
     }
     else if(option==2){
         let carReg:string=readlineSync.question("insert car registration ");
         carReg.toUpperCase;
-        registry.searchAutobyCarRegistration(carReg);
+        registry.searchAutobyCarRegistration(carReg); //busco un elemento en el registro con la patente del elemento
     }
     else if (option == 3) {
         auto1 = completeCar();
-        registry.insertAuto(auto1);
+        registry.insertAuto(auto1); //inserto un elemento Auto en el registro
     }
     else if (option == 4) {
         auto1 = completeCar();
-        registry.deleteAuto(auto1);
+        registry.deleteAuto(auto1); //busco un elemento del registro y lo elimino
     }
     else if (option == 5) {
 
-        console.log(registry);
+        console.log(registry); // imprimo el registro completo
     }
 
     option = readlineSync.questionInt("Wich option do you want to use? press 1 for search by features,2 for search by car registration, 3 for insert, 4 for delete, 5 for print all the registy, 0 for exit ");
